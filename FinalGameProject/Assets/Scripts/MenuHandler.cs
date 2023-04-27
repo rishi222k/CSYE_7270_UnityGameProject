0087e62c5e5260f5e1eac6f7a3602e1c9a40cbfa
Rishi Raghu <rishiraghu22@yahoo.com>
2023-04-27 12:48:09 -0400

    Main Menu UI

---
+++ FinalGameProject/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private string whichScene;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(whichScene);
+    }
+}
